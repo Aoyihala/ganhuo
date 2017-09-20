667335811ad06caafb26807d2492b53f57e15487
Aoyihala <1214504624@qq.com>
2017-09-20 10:17:56 +0800

    其他工具

---
+++ app/src/main/java/com/p2p/dsad/ganhuo/utlis/Utils.java
@@ -0,0 +1,78 @@
+package com.p2p.dsad.ganhuo.utlis;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Point;
+import android.os.Build;
+import android.text.TextUtils;
+import android.view.Display;
+import android.view.WindowManager;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *工具
+ */
+public class Utils {
+    private static int screenWidth = 0;
+    private static int screenHeight = 0;
+
+    public static int dpToPx(int dp) {
+        return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
+    }
+
+    public static int getScreenHeight(Context c) {
+        if (screenHeight == 0) {
+            WindowManager wm = (WindowManager) c.getSystemService(Context.WINDOW_SERVICE);
+            Display display = wm.getDefaultDisplay();
+            Point size = new Point();
+            display.getSize(size);
+            screenHeight = size.y;
+        }
+
+        return screenHeight;
+    }
+
+    public static int getScreenWidth(Context c)
+    {
+        if (screenWidth == 0)
+        {
+            WindowManager wm = (WindowManager) c.getSystemService(Context.WINDOW_SERVICE);
+            Display display = wm.getDefaultDisplay();
+            Point size = new Point();
+            display.getSize(size);
+            screenWidth = size.x;
+        }
+
+        return screenWidth;
+    }
+
+    public static boolean isAndroid5() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
+    }
+
+    /***
+     * 获取默认格式的日期字符串
+     * @param date
+     * @return
+     */
+    public static String getFormatDateStr(final Date date){
+        if(null == date)return null;
+        return DateFormat.getDateInstance(DateFormat.DEFAULT).format(date);
+    }
+
+    public static Date formatDateFromStr(final String dateStr){
+        Date date = new Date();
+        if(!TextUtils.isEmpty(dateStr)){
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.sss'Z'");
+            try {
+                date = sdf.parse(dateStr);
+            }catch (Exception e){
+                System.out.print("Error,format Date error");
+            }
+        }
+        return date;
+    }
+}

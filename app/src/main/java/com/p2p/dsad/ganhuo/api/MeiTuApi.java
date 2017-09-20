86372968edcb5f8422d74f07f3d425a09ca1873a
Aoyihala <1214504624@qq.com>
2017-09-20 10:10:22 +0800

    图片的api

---
+++ app/src/main/java/com/p2p/dsad/ganhuo/api/MeiTuApi.java
@@ -0,0 +1,29 @@
+package com.p2p.dsad.ganhuo.api;
+
+/**
+ * 来自必应的美图
+ * Created by dsad on 2017/9/14.
+ */
+
+public class MeiTuApi
+{
+    //切图url格式:http://tu.ihuan.me/api/image.php?url=http://xxx.xxx&w=xxx&h=xxx
+    //还带小故事
+    public static final String MEITU = "http://tu.ihuan.me/api/me_all_story_json";
+    //切刀(失效)
+    public static final String IMG_WEDITH="w";
+    public static final String DEFAUT_WEDITH="800";
+    public static final String DEFAUT_HEIGHT="600";
+    public static final String IMG_URL="url";
+    public static final String IMG_HEIGHT="h";
+    public static final String IMAGKINEF = "http://tu.ihuan.me/api/image.php";
+    //真随机图
+    public static final String DONGMANIMG="http://acg.bakayun.cn/randbg.php";
+    //真随机图2
+    public static final String DONGMANIMG2="https://api.ikmoe.com/moeu-rand-background.php";
+    //真随机图3
+    public static final String DONGMANIMG3="http://api.ikll.me/php.php";
+    //必应每日
+    public static final String BINGYING = "http://api.dujin.org/bing/1366.php";
+
+}

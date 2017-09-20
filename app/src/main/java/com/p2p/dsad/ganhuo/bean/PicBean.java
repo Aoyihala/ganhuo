2c286b0334696ce7a309c9cc9348df9077191887
Aoyihala <1214504624@qq.com>
2017-09-20 10:11:07 +0800

    实体2

---
+++ app/src/main/java/com/p2p/dsad/ganhuo/bean/PicBean.java
@@ -0,0 +1,126 @@
+package com.p2p.dsad.ganhuo.bean;
+
+/**
+ * 图片加小故事(作为侧滑菜单的头部图片小故事)
+ * Created by dsad on 2017/9/14.
+ */
+
+public class PicBean
+{
+
+    /**
+     * id : 336
+     * link : http://7vzpfb.com1.z0.glb.clouddn.com/bgc/16-04-30.png
+     * source : 小幻bing抓取
+     * sourcelink : http://www.bing.com
+     * time : 2016-04-30
+     * describe : 美国德克萨斯州，瓜达洛普山国家公园上空的卷云 (© Viktor Posnov/Getty Images)
+     * title : 高山上的卷云
+     * d : 翠绿山脉延绵不绝，峡谷深邃，洞穴神秘。天空湛蓝且高远，在这里的每一口呼吸都是那么沁人心脾。高山上，洁白的卷云一朵朵地盘旋着，来到这里，就让人无法挪步，想要永远呆下去。
+     * date : April 30
+     * attribute : 美国，德克萨斯州
+     */
+
+    private String id;
+    private String link;
+    private String source;
+    private String sourcelink;
+    private String time;
+    private String describe;
+    private String title;
+    private String d;
+    private String date;
+    private String attribute;
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public void setId(String id)
+    {
+        this.id = id;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link)
+    {
+        this.link = link;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source)
+    {
+        this.source = source;
+    }
+
+    public String getSourcelink()
+    {
+        return sourcelink;
+    }
+
+    public void setSourcelink(String sourcelink)
+    {
+        this.sourcelink = sourcelink;
+    }
+
+    public String getTime()
+    {
+        return time;
+    }
+
+    public void setTime(String time)
+    {
+        this.time = time;
+    }
+
+    public String getDescribe()
+    {
+        return describe;
+    }
+
+    public void setDescribe(String describe)
+    {
+        this.describe = describe;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public String getD() {
+        return d;
+    }
+
+    public void setD(String d) {
+        this.d = d;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getAttribute() {
+        return attribute;
+    }
+
+    public void setAttribute(String attribute) {
+        this.attribute = attribute;
+    }
+}
